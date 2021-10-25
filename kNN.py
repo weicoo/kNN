501a3742443e8c945c946619a6b99e5e796b8fc3
weicoo <785994138@qq.com>
2021-10-25 16:49:39 +0800

    find dataset and add it

--- kNN.py
+++ kNN.py
@@ -25,3 +25,18 @@ def classify0(inX, dataSet, labels, k):
 
 #group,labels = createDataSet()
 #classify0([0,0],group,labels,3)
+
+def file2matrix(filename) :
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
